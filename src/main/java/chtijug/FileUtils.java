ad772e86f061c5a1d738af962c43336857cea6c1
awillemant <amaury.willemant@atos.net>
2015-05-06 16:07:39 +0200

    first commit

---
+++ src/main/java/chtijug/FileUtils.java
@@ -0,0 +1,74 @@
+package chtijug;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FileUtils {
+
+    private final static Pattern FILENAME_PATTERN = Pattern.compile("slide_([^_]+)_([^_]+)\\.html");
+
+
+    public static Consumer<? super Path> printLines(PrintWriter writer) {
+        return p -> {
+            try {
+                List<String> lines = Files.readAllLines(p, Charset.forName("UTF-8"));
+                writer.print("<section>");
+                lines.forEach(writer::print);
+                writer.print("</section>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+    }
+
+
+    public static Path getResourcesPath(String folderName) {
+        String filePath = Thread.currentThread().getContextClassLoader().getResource(folderName).getFile();
+        filePath = convertPath(filePath);
+        return Paths.get(filePath);
+    }
+
+
+    public static String convertPath(String filePath) {
+        String osName = System.getProperty("os.name");
+        if (osName.toLowerCase().contains("windows")) {
+            filePath = filePath.replaceAll("/", File.separator + File.separator + File.separator + File.separator);
+            filePath = filePath.substring(2);
+        }
+        return filePath;
+    }
+
+
+    public static Map<String, List<Path>> listSourceFiles(String directory, String filePattern) throws IOException {
+        Path dir = getResourcesPath(directory);
+        Map<String, List<Path>> result = new TreeMap<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, filePattern)) {
+            for (Path entry : stream) {
+                Matcher matcher = FILENAME_PATTERN.matcher(entry.getFileName().toString());
+                matcher.find();
+                String cle = matcher.group(1);
+                if (!result.containsKey(cle)) {
+                    result.put(cle, new ArrayList<>());
+                }
+                result.get(cle).add(entry);
+            }
+        } catch (DirectoryIteratorException ex) {
+            throw ex.getCause();
+        }
+        return result;
+    }
+}
---
+++ src/main/java/chtijug/SlidesProvider.java
@@ -0,0 +1,41 @@
+package chtijug;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/slides/*")
+public class SlidesProvider extends HttpServlet {
+
+    private static final String SLIDES = "[[SLIDES]]";
+
+    private static final long serialVersionUID = 189098706874984983L;
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter writer = resp.getWriter();
+        Path indexPath = FileUtils.getResourcesPath("index.html");
+        String indexContent = new String(Files.readAllBytes(indexPath), "UTF-8");
+        int beforeIndex = indexContent.indexOf(SLIDES);
+        String before = indexContent.substring(0, beforeIndex);
+        writer.print(before);
+        Map<String, List<Path>> paths = FileUtils.listSourceFiles("slides", "slide_*.html");
+        paths.forEach((k, v) -> {
+            writer.print("<section>");
+            v.forEach(FileUtils.printLines(writer));
+            writer.print("</section>");
+        });
+        String after = indexContent.substring(beforeIndex + SLIDES.length());
+        writer.print(after);
+    }
+}
