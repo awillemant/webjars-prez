5011b23cfae76d57e61144579ab725a4bce4a8f7
awillemant <amaury.willemant@atos.net>
2015-05-20 13:48:30 +0200

    add jetty support

--- src/main/java/chtijug/SlidesProvider.java
+++ src/main/java/chtijug/SlidesProvider.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/slides/*")
+@WebServlet(urlPatterns = "/slides")
 public class SlidesProvider extends HttpServlet {
 
     private static final String SLIDES = "[[SLIDES]]";
